761922346df9a3c634d99fad769ee50135eafde9
unknown <anhmuoi200147@gmail.com>
2021-05-08 18:10:16 +0700

    xu ly cm jq

--- 1.js
+++ 1.js
@@ -1,6 +1,9 @@
 ﻿ $(function(){
-     $(".xuong,.kn,.sp,.tt,.cm").click(function(){
+     $(".xuong,.kn,.sp,.tt").click(function(){
          $('body,html').animate({scrollTop:$('#khoiedu').offset().top},800)
      })
+     $(".cm").click(function(){
+        $('body,html').animate({scrollTop:$('#phanhoi').offset().top},800)
+    })
 })  
  
